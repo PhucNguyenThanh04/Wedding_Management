f9cc5a0c88255efac2fe52fc38739973b8f8b7f8
PhucNguyenThanh04 <thanhphucnguyen101204@gmail.com>
2026-03-20 00:15:37 +0700

    forgotpw

--- backend/src/main.py
+++ backend/src/main.py
@@ -41,7 +41,7 @@ else:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost:3000","http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
